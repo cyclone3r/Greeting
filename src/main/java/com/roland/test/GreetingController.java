25159a2a7175bc4e8540f17622510a11514feaa1
cyclone3r <cyclone3r@gmail.com>
2022-06-15 14:00:22 -0700

    greeting firestore

--- src/main/java/com/roland/test/GreetingController.java
+++ src/main/java/com/roland/test/GreetingController.java
@@ -1,72 +1,52 @@
 package com.roland.test;
 
-import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private static Map<Integer, Greeting> greetingList = new HashMap<>();
-	
+    private static final String template = "Hello, %s!";
+    private static Map<Integer, Greeting> greetingList = new HashMap<>();
 
-	@GetMapping("/greeting")
-	public Map<Integer, Greeting> greetings(@RequestParam(value = "param1", required = false) String param1,
-			@RequestParam(value = "param2", required = false) String param2 ) {
-		 System.out.println(param1 + ", " + param2);
-		return greetingList; //new GreetingsDao(greetingList);
-	}
-	
-	@GetMapping("/greeting/{id}")
-	public Greeting greetings(@PathVariable(value = "id") int id) {
-		
-		return greetingList.get(id);
-	}
-	
-	@PostMapping("/greeting")
-	public void greeting(@RequestBody Greeting greeting) {
-		 greetingList.put(greeting.getId(), greeting);
-		 System.out.println(greetingList.size());
-	}
-	
-	@PutMapping("/greeting")
-	public void greetingUpdate(@RequestBody Greeting greeting) {
-		 greetingList.put(greeting.getId(), greeting);
-		 System.out.println("update: " + greetingList.size());
-	}
-	
-	@DeleteMapping("/greeting/{id}")
-	public void greetingDelete(@PathVariable(value = "id") int id) {
-		 greetingList.remove(id);
-		 System.out.println("update: " + greetingList.size());
-	}
-	
-	/*
-	@PutMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(1, String.format(template, name));
-	}
-	
-	@PatchMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(1, String.format(template, name));
-	}
-	
-	@DeleteMapping("/greeting")
-	public Greeting deleteGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(1, String.format(template, name));
-	} */
+    @Autowired
+    private GreetingService service;
+
+
+    @GetMapping("/greeting")
+    public Map<String, Greeting> greetings(@RequestParam(value = "param1", required = false) String param1,
+                                            @RequestParam(value = "param2", required = false) String param2) {
+        System.out.println(param1 + ", " + param2);
+        return service.retrieveGreetings();//greetingList; //new GreetingsDao(greetingList);
+    }
+
+    @GetMapping("/greeting/{id}")
+    public Greeting greetings(@PathVariable(value = "id") int id) {
+
+        return greetingList.get(id);
+    }
+
+    @PostMapping("/greeting")
+    public void greetingAdd(@RequestBody Greeting greeting) {
+        greetingList.put(greeting.getId(), greeting);
+        System.out.println(greetingList.size());
+        try {
+            service.addGreeting(greeting);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PutMapping("/greeting")
+    public void greetingUpdate(@RequestBody Greeting greeting) {
+        greetingList.put(greeting.getId(), greeting);
+        service.updateGreeting(greeting);
+    }
+
+    @DeleteMapping("/greeting/{id}")
+    public void greetingDelete(@PathVariable(value = "id") String id) {
+        service.deleteGreeting(id);
+    }
 }
---
+++ src/main/java/com/roland/test/GreetingService.java
@@ -0,0 +1,86 @@
+package com.roland.test;
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.*;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+
+@Service
+public class GreetingService {
+    public void addGreeting(Greeting greeting) throws IOException, ExecutionException, InterruptedException {
+
+        System.out.println("Insert Greeting");
+        DocumentReference ref = retrieveDB().collection("Greeting").document(String.valueOf(greeting.getId()));
+        ApiFuture<WriteResult> result = ref.set(greeting);
+
+        System.out.println("Update time : " + result.get().getUpdateTime());
+    }
+
+    public void updateGreeting(Greeting greeting){
+        System.out.println("Update Greeting");
+        DocumentReference ref = retrieveDB().collection("Greeting").document(String.valueOf(greeting.getId()));
+        ApiFuture<WriteResult> result = ref.set(greeting);
+
+        try {
+            System.out.println("Update time : " + result.get().getUpdateTime());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Map<String, Greeting> retrieveGreetings(){
+        Map<String, Greeting> greetings = new HashMap<>();
+        ApiFuture<QuerySnapshot> query = retrieveDB().collection("Greeting").get();
+
+        QuerySnapshot querySnapshot = null;
+        try {
+            querySnapshot = query.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for(QueryDocumentSnapshot document : documents){
+            greetings.put(document.getId(), document.toObject(Greeting.class));
+        }
+
+        return greetings;
+    }
+
+    public void deleteGreeting(String greetingId){
+        System.out.println("Delete Greeting");
+
+        ApiFuture<WriteResult> result  = retrieveDB().collection("Greeting").document(greetingId).delete();
+        try {
+            System.out.println(result.get().getUpdateTime());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+    public Firestore retrieveDB(){
+        try{
+            FirestoreOptions firestoreOptions =
+                    FirestoreOptions.getDefaultInstance().toBuilder()
+                            .setProjectId("sapient-symbol-261121")
+                            .setCredentials(GoogleCredentials.getApplicationDefault())
+                            .build();
+            Firestore db = firestoreOptions.getService();
+
+            return db;
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+         return null;
+    }
+}
